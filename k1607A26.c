8545d720f8c4374a62821b63d3caabd26514186d
Shah Alam Ansari <shahebansari9@gmail.com>
2018-04-15 00:40:44 +0530

    Initialize values and priorities

--- k1607A26.c
+++ k1607A26.c
@@ -13,4 +13,28 @@ int main(int argc, char const *argv[])
 	scanf("%d",&n);
 	int query[n][2];
 	int burst_time[n];
+
+	for (int i = 0; i < n; ++i)
+	{
+		printf("\nQUERY: %d",i);
+		printf("\nAre you a student of teacher?: ");
+	    scanf("%s",str);
+		printf("\nEnter the time required for your query in minutes: ");
+		scanf("%d",&b_t);
+		if (str[0]=='s' || str[0]=='S')
+		{
+			query[i][0] = i;
+			query[i][1] = 1;
+			burst_time[i]=b_t;
+
+		}
+
+		if (str[0]=='t' || str[0]=='T')
+		{
+			query[i][0] = i;
+			query[i][1] = 0;
+			burst_time[i]=b_t;
+		}
+
+	}
 }
