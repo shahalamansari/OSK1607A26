305c28a2a65dcde88a613267ecaeea05026bde9d
Shah Alam Ansari <shahebansari9@gmail.com>
2018-04-15 00:36:21 +0530

    Ask number of queries

---
+++ k1607A26.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("\n***********SUDESH ONLINE 120 MINUTES QUERY PORTAL***************\n");
+	int time_quantum = 5;
+	int n,b_t;
+	char str[10];
+	printf("\nEnter the number of queries: ");
+	scanf("%d",&n);
+	int query[n][2];
+	int burst_time[n];
+}
